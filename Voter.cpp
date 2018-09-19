2950431f0c43c5cd5a3f50077873914ff59c6a90
mattschultz1337 <mschult7@binghamton.edu>
2018-09-19 18:21:18 -0400

    * nomo

--- Voter.cpp
+++ Voter.cpp
@@ -12,20 +12,20 @@ class Voter {
 
 int main(int argc, char *argv[])
 {
-   string lastname="";
-   string firstname="";
-   int age=0;
-   int stnum=0;
-   string street="";
-   string town="";
-   string zip="";
-   float amtdonated=0;
+   string* lastname="";
+   string* firstname="";
+   int* age=0;
+   int* stnum=0;
+   string* street="";
+   string* town="";
+   string* zip="";
+   float* amtdonated=0;
    cout << ">: ";
    string command;
    getline (cin, command);
    while(command != "Quit"){
 	if(command=="New"){
-		VoterDB::cmd_new(*lastname,*firstname,*age,*stnum,*street,*town,*zip,*amtdonated);
+		VoterDB::cmd_new(lastname,firstname,age,stnum,street,town,zip,amtdonated);
 	} else if(command=="Update"){
 		VoterDB::cmd_update(lastname, firstname, age, stnum,street,town,zip);
 	} else if(command=="View"){
