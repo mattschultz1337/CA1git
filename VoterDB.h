87e442c6002ceb2d0b028b190b49574de3326a65
unknown <mschult7@binghamton.edu>
2018-09-19 19:12:07 -0400

    change

--- VoterDB.h
+++ VoterDB.h
@@ -8,7 +8,7 @@
 using namespace std;
 class VoterDB {
    public:
-       static void cmd_new(std::string& lastname,std::string& firstname, int& age, int& stnum, std::string& streetname, std::string& town, std::string& zip, float& amtdonated);
+       static void cmd_new(std::string lastname,std::string& firstname, int& age, int& stnum, std::string& streetname, std::string& town, std::string& zip, float& amtdonated);
        static void cmd_update(std::string& lastname,std::string& firstname, int& age, int& stnum, std::string& streetname, std::string& town, std::string& zip);
       static void cmd_view(std::string& lastname,std::string& firstname, int& age, int& stnum, std::string& streetname, std::string& town, std::string& zip, float& amtdonated);
 
